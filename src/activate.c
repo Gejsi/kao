5fead237e462580badb14484e399eba5a0413b3c
Gejsi <vgejsi@gmail.com>
2024-08-20 16:59:01 +0200

    Replace gtk init with adwaita init

--- src/activate.c
+++ src/activate.c
@@ -5,16 +5,14 @@ static void print_hello(GtkWidget *widget, gpointer data) {
 }
 
 void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window;
-  GtkWidget *button;
-
-  window = gtk_application_window_new(app);
+  GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Hello");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 500);
   gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
   gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
   gtk_window_set_modal(GTK_WINDOW(window), TRUE);
 
+  GtkWidget *button;
   button = gtk_button_new_with_label("Hello World!!!");
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
   gtk_window_set_child(GTK_WINDOW(window), button);
--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include "activate.h"
-#include <gtk/gtk.h>
+#include <adwaita.h>
 
 int main(int argc, char **argv) {
-  GtkApplication *app;
-  int status;
+  g_autoptr(AdwApplication) app = NULL;
 
-  app = gtk_application_new("org.gtk.hello", G_APPLICATION_DEFAULT_FLAGS);
+  app = adw_application_new("org.example.Hello", G_APPLICATION_DEFAULT_FLAGS);
+
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
 
-  return status;
+  return g_application_run(G_APPLICATION(app), argc, argv);
 }
