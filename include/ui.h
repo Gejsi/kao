e82a8a8aa1e44b00aad7a7667ecd267578350e3d
Gejsi <vgejsi@gmail.com>
2024-08-24 16:09:26 +0200

    Added grid of buttons

--- include/ui.h
+++ include/ui.h
@@ -7,5 +7,6 @@ typedef enum { All, Happy, Sad, Angry, Misc } Expr;
 
 GtkWidget *build_ui_wrapper(void);
 GtkWidget *build_scrollable_tab(Expr expr);
+GtkWidget *build_grid(Expr expr);
 
 #endif // UI_H
--- src/ui.c
+++ src/ui.c
@@ -2,7 +2,7 @@
 #include "gtk/gtk.h"
 #include <glib.h>
 
-const int PADDING = 16;
+const int ROOT_SIZE = 16;
 
 GtkWidget *build_ui_wrapper(void) {
   GtkWidget *notebook = gtk_notebook_new();
@@ -23,35 +23,13 @@ GtkWidget *build_ui_wrapper(void) {
 
 GtkWidget *build_scrollable_tab(Expr expr) {
   GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-  gtk_widget_set_margin_top(box, PADDING);
-  gtk_widget_set_margin_bottom(box, PADDING);
-  gtk_widget_set_margin_start(box, PADDING);
-  gtk_widget_set_margin_end(box, PADDING);
+  gtk_widget_set_margin_top(box, ROOT_SIZE);
+  gtk_widget_set_margin_bottom(box, ROOT_SIZE);
+  gtk_widget_set_margin_start(box, ROOT_SIZE);
+  gtk_widget_set_margin_end(box, ROOT_SIZE);
 
-  const gchar *content;
-  switch (expr) {
-  case All:
-    content = "This is the entire content!";
-    break;
-  case Happy:
-    content = "This is the happy content!";
-    break;
-  case Sad:
-    content = "This is the sad content.";
-    break;
-  case Angry:
-    content = "This is the angry content!";
-    break;
-  case Misc:
-    content = "This is the misc content!";
-    break;
-  }
-
-  GtkWidget *label = gtk_label_new(content);
-  gtk_label_set_wrap(GTK_LABEL(label), TRUE);
-  gtk_label_set_xalign(GTK_LABEL(label), 0.0);
-
-  gtk_box_append(GTK_BOX(box), label);
+  GtkWidget *grid = build_grid(expr);
+  gtk_box_append(GTK_BOX(box), grid);
 
   GtkWidget *scrolled_window = gtk_scrolled_window_new();
   gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), box);
@@ -59,4 +37,60 @@ GtkWidget *build_scrollable_tab(Expr expr) {
                                  GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 
   return scrolled_window;
+}
+
+void handle_copy(GtkWidget *button, gpointer data) {
+  g_print("Button clicked: %s\n", gtk_button_get_label(GTK_BUTTON(button)));
+}
+
+GtkWidget *build_grid(Expr expr) {
+  GtkWidget *grid = gtk_grid_new();
+  gtk_grid_set_row_spacing(GTK_GRID(grid), ROOT_SIZE * 0.5);
+  gtk_grid_set_column_spacing(GTK_GRID(grid), ROOT_SIZE * 0.5);
+
+  const int GRID_LENGTH = 3;
+  const gchar *button_labels[GRID_LENGTH][GRID_LENGTH];
+
+  if (expr == Happy) {
+    button_labels[0][0] = ":)";
+    button_labels[0][1] = ":D";
+    button_labels[0][2] = "😊";
+    button_labels[1][0] = "😄";
+    button_labels[1][1] = "😁";
+    button_labels[1][2] = "😃";
+    button_labels[2][0] = "😆";
+    button_labels[2][1] = "😂";
+    button_labels[2][2] = "😹";
+  } else if (expr == Sad) {
+    button_labels[0][0] = ":(";
+    button_labels[0][1] = ":'(";
+    button_labels[0][2] = "😢";
+    button_labels[1][0] = "😭";
+    button_labels[1][1] = "😞";
+    button_labels[1][2] = "😔";
+    button_labels[2][0] = "😟";
+    button_labels[2][1] = "😕";
+    button_labels[2][2] = "😣";
+  } else if (expr == Angry) {
+    button_labels[0][0] = ">:(";
+    button_labels[0][1] = "😠";
+    button_labels[0][2] = "😡";
+    button_labels[1][0] = "😤";
+    button_labels[1][1] = "😾";
+    button_labels[1][2] = "😒";
+    button_labels[2][0] = "👿";
+    button_labels[2][1] = "💢";
+    button_labels[2][2] = "😑";
+  }
+
+  for (int i = 0; i < GRID_LENGTH; i++) {
+    for (int j = 0; j < GRID_LENGTH; j++) {
+      GtkWidget *button = gtk_button_new_with_label(button_labels[i][j]);
+      gtk_widget_set_hexpand(button, TRUE);
+      g_signal_connect(button, "clicked", G_CALLBACK(handle_copy), NULL);
+      gtk_grid_attach(GTK_GRID(grid), button, j, i, 1, 1);
+    }
+  }
+
+  return grid;
 }
